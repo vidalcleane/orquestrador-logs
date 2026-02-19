9b00df7287fe2a54ed2fa67a0ee786c562782183
Cleane Vidal <mcleanevidalf@gmail.com>
2026-02-19 08:12:21 -0300

    fix python output buffer

--- worker-python/worker.py
+++ worker-python/worker.py
@@ -3,7 +3,6 @@ import random
 import time
 import os
 
-import os
 API_URL = os.environ.get("API_URL", "http://api-dotnet:5000/webhook/preco")
 
 while True:
@@ -12,8 +11,8 @@ while True:
     
     try:
         response = requests.post(API_URL, json=payload)
-        print(f"Enviado: R${preco} | Status: {response.status_code}")
+        print(f"Enviado: R${preco} | Status: {response.status_code}", flush=True)
     except Exception as e:
-        print(f"Erro ao enviar: {e}")
+        print(f"Erro ao enviar: {e}", flush=True)
     
     time.sleep(5)
